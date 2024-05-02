9740ff2074aae724ccf1a45a0d01678d02f73c58
FelipeJU22 <112335803+FelipeJU22@users.noreply.github.com>
2024-05-01 22:35:16 -0600

    Se añade la base de datos local y procedimientos

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/login';
 import Home from './screens/home';
 import Account from './screens/resetPassword';
-
+import {asignarHorariosALaboratorios, borrarSolicitudes, crearBaseDeDatos, crearLaboratorios, insertarProfesores, insertarSolicitudes, obtenerInstanciasHorario,  } from './DataBase_SQLite/DBTables';
 
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  useEffect(() => {
+//AQUÍ VA LO QUE SE QUIERA CAMBIAR DE LA BASE DE DATOS
+
+
+  }, []); // Esto asegura que se ejecute solo una vez al inicio de la aplicación
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
