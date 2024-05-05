9a5000f6db6d086cd0689a859bcccacffed9f282
FelipeJU22 <112335803+FelipeJU22@users.noreply.github.com>
2024-05-04 22:20:49 -0600

    Sincronización con SQL

--- App.js
+++ App.js
@@ -1,23 +1,56 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/login';
 import Home from './screens/home';
 import Account from './screens/resetPassword';
-import {asignarHorariosALaboratorios, borrarSolicitudes, crearBaseDeDatos, crearLaboratorios, insertarProfesores, insertarSolicitudes, obtenerInstanciasHorario,  } from './DataBase_SQLite/DBTables';
+import {asignarHorariosALaboratorios, borrarHorario, borrarLaboratorio, 
+  borrarProfesores, borrarSolicitudes, crearBaseDeDatos, crearLaboratorios, 
+  insertarProfesores, insertarSolicitudes, obtenerInstanciasHorario, borrarTablas, 
+  borrarBaseDeDatos} from './DataBase_SQLite/DBTables';
 
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+
+  const [credencialesProfesores, setCredencialesProfesores] = useState([]);
+
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://192.168.100.251:5095/Profesor/CredencialesProfesores', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      setCredencialesProfesores(data);
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
+
+
+
+
   useEffect(() => {
 //AQUÍ VA LO QUE SE QUIERA CAMBIAR DE LA BASE DE DATOS
+    crearBaseDeDatos();
 
-
+    borrarProfesores();
+    borrarSolicitudes();
+    fetchData();
   }, []); // Esto asegura que se ejecute solo una vez al inicio de la aplicación
 
+  useEffect(() => {
+    insertarProfesores(credencialesProfesores);
+  }, [credencialesProfesores]);
   return (
     <NavigationContainer>
       <Stack.Navigator>
