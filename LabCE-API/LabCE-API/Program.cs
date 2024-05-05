e81bfd9633355c9350f6d8bf4744adb6da27c762
MauroCR52 <mauluna52@gmail.com>
2024-05-05 01:37:25 -0600

    pequeños cambios

--- LabCE-API/LabCE-API/Program.cs
+++ LabCE-API/LabCE-API/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddLabCEServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new() { Title = "Mi API", Version = "v1" });
+    c.SwaggerDoc("v1", new() { Title = "LabCE_API", Version = "v1" });
 
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
